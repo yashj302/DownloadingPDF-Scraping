b916d35f67b2e423ee684fb90d4f9127bef9b7e0
Yash Jain <yashj302@gmail.com>
2018-09-22 15:05:26 +0530

    Update ScrapingPDF.py

--- ScrapingPDF.py
+++ ScrapingPDF.py
@@ -7,7 +7,9 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = input("Enter URL you want to scrap:- ")  # For Testing enter https://doc.lagout.org/programmation/python/ in url
+url = input("Enter URL you want to scrap:- ")  
+# For Testing enter https://doc.lagout.org/programmation/python/ in url
+# and remove # from line 23 and comment 25th line
 pwd = input("Enter where to store : Example:'/home/yash/' include forward slahes too: ")
 html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html,'html.parser')
